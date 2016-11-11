2bd1cdfa0a6d67202afa93e58aff999976da208e
doc HD <doc.divxm@gmail.com>
2016-11-11 10:10:37 +0300

    angler: Enable TARGET_USES_AOSP flag
    Change-Id: Ib26b3413c28f3218cfb18141dac5fca94d74e0f2

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -127,13 +127,14 @@ BOARD_SEPOLICY_DIRS += \
 
 TARGET_USES_64_BIT_BINDER := true
 
+TARGET_USES_AOSP := true
+
 TARGET_USES_INTERACTION_BOOST := true
 
 # Force camera module to be compiled only in 32-bit mode on 64-bit systems
 # Once camera module can run in the native mode of the system (either
 # 32-bit or 64-bit), the following line should be deleted
 BOARD_QTI_CAMERA_32BIT_ONLY := true
-USE_DEVICE_SPECIFIC_CAMERA := true
 
 # Wifi related defines
 BOARD_WPA_SUPPLICANT_DRIVER := NL80211
