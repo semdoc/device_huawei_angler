9d69f9b86c5d4abb2c6c4185f315840e2ffddb53
Davor Bertovic <davor@losinj.com>
2018-11-18 19:50:05 +0100

    AICPfy
    Change-Id: I3040ed79f5946437b49f6e7241c1509e25488be1

--- device.mk
+++ device.mk
@@ -374,7 +374,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Write Manufacturer & Model information in created media files.
 # IMPORTANT: ONLY SET THIS PROPERTY TO TRUE FOR PUBLIC DEVICES
-ifneq ($(filter aosp_angler% gzosp_angler%, $(TARGET_PRODUCT)),)
+ifneq ($(filter aosp_angler% aicp_angler%, $(TARGET_PRODUCT)),)
 PRODUCT_PROPERTY_OVERRIDES += \
     media.recorder.show_manufacturer_and_model=true
 else
