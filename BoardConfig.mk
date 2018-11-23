ce5a468561c92c2846b04a3404b6f8cb92a7e7f6
doc HD <doc.divxm@gmail.com>
2018-11-24 01:17:36 +0300

    angler: Ignore selinux neverallows
    Change-Id: I39b0839755721e10fe722eab4339e43c86f79f0e

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -157,4 +157,6 @@ TARGET_FS_CONFIG_GEN += device/huawei/angler/config.fs
 DEVICE_MANIFEST_FILE := device/huawei/angler/manifest.xml
 DEVICE_MATRIX_FILE := device/huawei/angler/compatibility_matrix.xml
 
+SELINUX_IGNORE_NEVERALLOWS := true
+
 -include vendor/huawei/angler/BoardConfigVendor.mk
